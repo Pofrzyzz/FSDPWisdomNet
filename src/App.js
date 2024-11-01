d564c1afd319f90b4c8a2a0febc7c2595a16a90a
Kar Kang Garence Wong <s10262458@connect.np.edu.sg>
2024-11-02 03:15:56 +0800

    Created Contact Us Page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/help&support';
 import BookingPage from './pages/booking';
+import ContactUsPage from './pages/contactUs';
+import InQueuePage from './pages/InQueue';
 
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
       <Routes>
         <Route path="/HomePage" element={<HomePage />} />
         <Route path="/BookingPage" element={<BookingPage />} />
+        <Route path="/ContactUsPage" element={<ContactUsPage />} />
+        <Route path="/InQueuePage" element={<InQueuePage />} />
       </Routes>
     </Router>
   );
