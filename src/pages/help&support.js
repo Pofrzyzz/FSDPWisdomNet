52249393f18c3d61c01a65197db0cf995796fa42
Pofrzyzz <148027130+Pofrzyzz@users.noreply.github.com>
2024-11-04 07:15:15 +0800

    ChatbotFE
    Implemented Chatbot component
    Integrate chatbot component into homepage/help&support.js

--- src/pages/help&support.js
+++ src/pages/help&support.js
@@ -5,6 +5,7 @@ import ContactBooking from '../components/contactbooking';
 import Footer from '../components/footer';
 import Help from '../components/help';
 import search from '../images/search.svg';
+import Chatbot from '../components/chatbot';
 
 function HomePage() {
   return (
@@ -12,8 +13,8 @@ function HomePage() {
       <Navbar />
       <section className="bg-white py-16">
         <div className="container mx-auto px-6 md:px-12 text-center">
-        <h1 className="text-3xl md:text-5xl font-bold mt-28 md:mt-28">Help & Support</h1>
-          
+          <h1 className="text-3xl md:text-5xl font-bold mt-28 md:mt-28">Help & Support</h1>
+
           {/* Search Bar */}
           <div className="relative mx-auto max-w-xl mt-6">
             <input 
@@ -28,7 +29,7 @@ function HomePage() {
           </div>
           
           {/* Common FAQ Section */}
-          <CommonFaqs/>
+          <CommonFaqs />
           {/* View More Button */}
           <button className="mt-8 px-6 py-2 bg-red-600 text-white font-semibold rounded-md hover:bg-red-700 transition-colors">
             View More
@@ -43,6 +44,9 @@ function HomePage() {
       </section>
       
       <Footer />
+
+      {/* Chatbot Button and Interface */}
+      <Chatbot />
     </div>
   );
 }
