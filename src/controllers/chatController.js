96f74d22c2df617002e8c8e13d941d14832c6beb
Eddie <s10221021@connect.np.edu.sg>
2025-02-01 22:00:39 +0800

    Revert 2.0

--- src/controllers/chatController.js
+++ src/controllers/chatController.js
@@ -1,4 +1,5 @@
-const axios = require('axios');
+// chatController.js
+
 const chatSessions = {}; // In-memory storage for sessions
 const messagesStore = {}; // In-memory storage for messages
 const operator = {
@@ -8,26 +9,8 @@ const operator = {
   branchName: "OCBC Main Branch"
 };
 
-const verifyRecaptcha = async (captchaResponse) => {
-  const secretKey = "6Led0skqAAAAACCu9oJidz8cAzJjGM32m0pki1cw";
-  const url = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${captchaResponse}`;
-
-  try {
-    const { data } = await axios.post(url);
-    return data.success;
-  } catch (error) {
-    console.error("Error verifying reCAPTCHA:", error);
-    return false;
-  }
-};
-
 // Function to start a new chat session
-const requestChat = async (userId, name, nric, recaptchaToken) => {
-  const isHuman = await verifyRecaptcha(recaptchaToken);
-  if (!isHuman) {
-    throw new Error('reCAPTCHA verification failed');
-  }
-  
+const requestChat = (userId, name, nric) => {
   const chatId = `chat_${Date.now()}`; // Unique chat ID using timestamp
   chatSessions[chatId] = { userId, name, nric, chatId, operator };
   messagesStore[chatId] = [];
